6c8b0724ebd324af683d49120eb88d947e2d5c4c
hezhixiang <514342179@qq.com>
2020-08-08 17:16:03 +0800

    修改 view 模板

--- 文件模板/UIViewObjective-C/___FILEBASENAME___.m
+++ 文件模板/UIViewObjective-C/___FILEBASENAME___.m
@@ -2,6 +2,12 @@
 
 #import "___FILEBASENAME___.h"
 
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+
+@end
+
+
 @implementation ___FILEBASENAMEASIDENTIFIER___
 
 - (instancetype)initWithFrame:(CGRect)frame {
