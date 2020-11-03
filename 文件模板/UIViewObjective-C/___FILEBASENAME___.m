c8ced4f06639f91701d036e49ddaaa307b121626
hezhixiang <514342179@qq.com>
2020-11-03 13:58:40 +0800

    添加新代码

--- 文件模板/UIViewObjective-C/___FILEBASENAME___.m
+++ 文件模板/UIViewObjective-C/___FILEBASENAME___.m
@@ -19,6 +19,19 @@
     return self;
 }
 
+- (instancetype)initWithReuseIdentifier:(nullable NSString *)reuseIdentifier {
+    if (self = [super initWithReuseIdentifier:reuseIdentifier]) {
+        if (@available(iOS 14.0, *)) {
+            self.backgroundConfiguration = [UIBackgroundConfiguration clearConfiguration];
+        }
+        
+        [self setupUI];
+    }
+    
+    return self;
+}
+
+
 - (void)setupUI {
     
 }
